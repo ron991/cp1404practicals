6e16e7f39f8a2167877425f4df2e192bdc623b6f
rkell <ronald.kelly@my.jcu.edu.au>
2024-06-06 21:59:21 +1000

    Add to prac_03 folder

---
+++ prac_03/string_formatting.py
@@ -0,0 +1,12 @@
+# 1922 Gibson L-5 CES for about $16,036!
+
+def main():
+    """This function will print"""
+    year_of_guitar = 1922
+    type_of_guitar = "Gibson"
+    model_of_guitar = "L-5 CES"
+    price_of_guitar = "$16,036"
+    print(f"{year_of_guitar} {type_of_guitar} {model_of_guitar} for about {price_of_guitar:}!")
+
+
+main()
