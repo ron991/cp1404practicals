617943124b4d264b0cf8937a749f82400b92757e
rkell <ronald.kelly@my.jcu.edu.au>
2024-06-06 21:39:52 +1000

    Removed comment

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -3,8 +3,6 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# TODO: Fix this!
-
 score = float(input("Enter score: "))
 if 0 < score > 100:
     print("Invalid score")
