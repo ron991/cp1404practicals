ba710ddcfb619d660d6048fcbc800d50c82d9c8b
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-04 22:51:42 +1000

    Add file to prac_03 directory

---
+++ prac_03/randoms.py
@@ -0,0 +1,11 @@
+import random
+print(random.randint(5, 20))  # output smallest 5 , largest 16
+
+print(random.randrange(3, 10, 2))  # output  smallest 5 , largest 9. There is no chance it would
+# get to for because the count starts at 3 with a 2 count increment.
+
+print(random.uniform(2.5, 5.5))  # output smallest 2.974443526483087, largest 5.168563002599675
+
+
+print(random.randrange(0,100,1))
+#Write code, not a comment, to produce a random number between 1 and 100 inclusive
