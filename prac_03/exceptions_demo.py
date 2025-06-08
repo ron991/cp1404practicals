e7c9265fb4d31d339338713d9038b21b6163e854
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-08 13:53:49 +1000

    Add comments to answer questions

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -2,8 +2,11 @@
 CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur?
+# A value error will occur when the input is not a valid integer for either the numerator or denominator.
 2. When will a ZeroDivisionError occur?
+#Will happen when the denominator is zero.
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+# Yes by adding a loop to ask for a non-zero denominator before the division.
 """
 
 try:
