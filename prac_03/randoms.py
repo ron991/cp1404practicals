05487d367b0ded1d98730240717551f960a0adfd
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-08 20:07:29 +1000

    Change comment answer

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -2,7 +2,7 @@ import random
 print(random.randint(5, 20))  # output smallest 5 , largest 16
 
 print(random.randrange(3, 10, 2))  # output  smallest 5 , largest 9. There is no chance it would
-# get to for because the count starts at 3 with a 2 count increment.
+# get to four because the count starts at 3 with a 2 count increment.
 
 print(random.uniform(2.5, 5.5))  # output smallest 2.974443526483087, largest 5.168563002599675
 
