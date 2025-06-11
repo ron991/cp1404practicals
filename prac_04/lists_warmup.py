8a0332c4ae9514d330004766109d86c281ece6a3
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-11 21:22:05 +1000

    Add test code for console

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,1 +1,13 @@
 numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Values at 0 : 3 , 1 : 1, 2 : 4, 3 : 5 , 4 : 9 , 5 : 2 . It is a List
+
+numbers[0]
+numbers[-1]
+numbers[3]
+numbers[:-1]
+numbers[3:4]
+5 in numbers
+7 in numbers
+"3" in numbers
+numbers + [6, 5, 3]
