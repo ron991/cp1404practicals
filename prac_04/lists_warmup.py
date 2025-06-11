94417e18fcdfab63a7a700798c8092d964574f91
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-11 22:22:15 +1000

    Add comment to answer prac questions

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -2,12 +2,22 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 
 # Values at 0 : 3 , 1 : 1, 2 : 4, 3 : 5 , 4 : 9 , 5 : 2 . It is a List
 
-numbers[0]
-numbers[-1]
-numbers[3]
-numbers[:-1]
-numbers[3:4]
-5 in numbers
-7 in numbers
-"3" in numbers
-numbers + [6, 5, 3]
+# numbers[0] output 3
+# numbers[-1] output 2
+# numbers[3] output 1
+# numbers[:-1] output [3, 1, 4, 1, 5, 9] Remove last number
+# numbers[3:4] output [1]
+# 5 in numbers output True
+# 7 in numbers output False
+# "3" in numbers output False
+# numbers + [6, 5, 3] output Adds numbers to end of numbers list
+#
+# 1. Change the first element of numbers to "ten" (the string, not the number 10)
+# 2. Change the last element of numbers to 1
+# 3. Print all the elements from numbers except the first two (slice)
+# 4. Print whether 9 is an element of numbers
+
+# numbers[0] = "ten" output is 'ten'
+# numbers [-1] = 1 output is 1
+# numbers [2:] output is [4, 1, 5, 9, 1]
+# 9 in numbers output True
