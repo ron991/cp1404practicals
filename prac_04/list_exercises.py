f80fb6f51ae71580021d752cfc31e4f33d0dc2f1
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-12 20:54:52 +1000

    Add new list of usernames to program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -23,6 +23,9 @@ for i in range(5):
 
 print(f"The first number is: {numbers[0]}")
 print(f"The last number is: {numbers[-1]}")
+print(f"The smallest number is: {min(numbers)}")
 print(f"The largest number is: {max(numbers)}")
 print(f"The average sum of numbers: {sum(numbers)}")
-print(f"The average sum of numbers: {sum(numbers)/len(numbers)}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
