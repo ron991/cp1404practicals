7bc5d6b0bfc8c3949ea0aa58f334f4d154ef6282
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-12 20:10:55 +1000

    Refactor print report from main to individual function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -14,12 +14,16 @@ def main():
         income = float(input(f"Enter income for month {month}  : "))
         incomes.append(income)
 
+    print_report(incomes, number_of_months)
 
+
+def print_report(incomes, number_of_months):
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
 
 main()
