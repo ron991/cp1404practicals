a6b9176dac16ea2418aafa3419b6482417bb4d65
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-12 20:15:11 +1000

    Add docstring to print_report function for a description of the function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -18,6 +18,7 @@ def main():
 
 
 def print_report(incomes, number_of_months):
+    """Print report for incomes over a given number of months."""
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
