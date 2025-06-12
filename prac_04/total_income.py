62bcd7090c156807450305e1c93ec14387416ef5
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-12 20:07:13 +1000

    Use f-string in main function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -10,9 +10,10 @@ def main():
     months = int(input("How many months? "))
 
     for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
-        # income = float(input(f"Enter income for month  + {month} +  : "))
+        # income = float(input("Enter income for month " + str(month) + ": "))
+        income = float(input(f"Enter income for month {month}  : "))
         incomes.append(income)
+
 
     print("\nIncome Report\n-------------")
     total = 0
@@ -20,6 +21,5 @@ def main():
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
-
 
 main()
