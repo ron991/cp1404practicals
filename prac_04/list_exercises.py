67c4a8fe077f1de2fc92fea44c029b15a052f0e2
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-12 21:03:10 +1000

    Remove sample output comment and question comment

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,18 +1,4 @@
-# Write a program that prompts the user for 5 numbers and then stores each of these in a list called numbers.
-# The program should then output information about these numbers, as in the output below.
-# You can use the functions min, max, sum and len, and you can use the append method to add a number to a list.
 
-#
-# Number: 5
-# Number: 20
-# Number: 1
-# Number: 2
-# Number: 3
-# The first   number is 5
-# Thelastnumber is 3
-# Thesmallestnumber is 1
-# Thelargestnumber is 20
-# The average of the numbers is 6.2
 
 # Basic List Operations
 numbers = []
