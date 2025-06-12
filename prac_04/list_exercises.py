73f9a6f7fd5c0fbae8b1be7dcd6e550bf59e9551
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-12 21:07:33 +1000

    Add username check to file

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -12,5 +12,13 @@ print(f"The smallest number is: {min(numbers)}")
 print(f"The largest number is: {max(numbers)}")
 print(f"The average sum of numbers: {sum(numbers)}")
 
+# Woefully inadequate security checker
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter the username: ")
+if username in usernames:
+    print(f"Access granted: {username}.")
+else:
+    print(f"Access denied: {username}.")
+
