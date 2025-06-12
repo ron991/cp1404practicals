7da95190cdb762fa60cbc60dc93ad7eb4896b6c0
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-12 22:37:23 +1000

    Copy and comment question for guidance on output

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,20 @@
+# Write a program that asks the user how many "quick picks" they wish to generate. The program then generates that many lines of output. Each line consists of 6 random numbers between 1 and 45.
+# These values should be stored as CONSTANTS.
+#
+# Each line (quick pick) should not contain any repeated number.
+# Each line of numbers should be displayed in sorted (ascending) order.
+# Study the formatting below so that numbers align neatly.
+# Note
+#
+# Python's random module has a sample() function, which returns a selection from a list.
+# This is a nice way to solve this problem... but it's too easy :)
+# Do not use it for this program.
+#
+# Your code should produce output that matches this sample output (except the numbers are random):
+#
+# How many quick picks? 5
+#  1 12 14 15 30 36
+#  2  5  8 33 38 41
+#  2 12 19 22 29 43
+# 13 21 28 29 42 43
+#  3  4 10 11 32 44
