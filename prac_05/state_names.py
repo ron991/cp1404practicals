ccf6158bb6adca54ffd50ba9117545978846e906
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-21 23:14:59 +1000

    Change f-string to format print output

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -8,7 +8,7 @@ File needs reformatting
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 for code, name in CODE_TO_NAME.items():
-    print(code, name)
+    print(f"{code:3} is {name}")
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
