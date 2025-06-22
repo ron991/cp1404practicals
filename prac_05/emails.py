7869a2fbea7afca198060b9d7e605aa24059eae8
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-22 14:43:28 +1000

    Complete main function

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -9,8 +9,21 @@ def main():
     email_to_name = {}
     email = input("Enter email: ")
     while email != "":
-        name_parts = email.split("@")[0]
+        name_part = email.split("@")[0]
         name_parts = name_part.replace('.', ' ').split()
         name = " ".join(name_parts).title()
+
+        confirmation = input(f"Is your name {name}? (Y/n) ").lower()
+        if confirmation not in ("", "y"):
+            email = input("Enter email: ")
+
+        email_to_name[email] = name
+        email = input("Enter email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+main()
 
 
