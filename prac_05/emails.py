d9638ba33c39d16be1c2d2af20fa5a4821d096f7
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-22 14:44:34 +1000

    Refactor to create extract_name_from_email function for cleaner readability

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -9,9 +9,7 @@ def main():
     email_to_name = {}
     email = input("Enter email: ")
     while email != "":
-        name_part = email.split("@")[0]
-        name_parts = name_part.replace('.', ' ').split()
-        name = " ".join(name_parts).title()
+        name = extract_name_from_email(email)
 
         confirmation = input(f"Is your name {name}? (Y/n) ").lower()
         if confirmation not in ("", "y"):
@@ -22,6 +20,13 @@ def main():
 
     for email, name in email_to_name.items():
         print(f"{name} ({email})")
+
+
+def extract_name_from_email(email):
+    name_part = email.split("@")[0]
+    name_parts = name_part.replace('.', ' ').split()
+    name = " ".join(name_parts).title()
+    return name
 
 
 main()
