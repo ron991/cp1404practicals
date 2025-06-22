8c85ca2d6e4e96ce6938413f5fef4a4972321aba
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-22 20:32:34 +1000

    Test read_data function for output

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -3,16 +3,23 @@ Wimbledon
 Estimated time to complete: 2 hours
 Actual time to complete:
 """
+import csv
 
 FILENAME = "wimbledon.csv"
 
 
 def main():
     """Read data and print details of wimbledon champions and what countries have won."""
+    data = read_data(FILENAME)
 
+def read_data(filename):
+    """read data from csv."""
+    with open(filename, encoding="utf-8-sig") as infile:
+        infile.readline()
+        #test
+        print(infile.readline())
 
 main()
 
 
-def load_data(filename):
-    """Load data from csv."""
+
