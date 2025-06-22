f1ea8a98c166cbdaeea624419a599470f66bc9ce
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-22 20:56:19 +1000

    Format code and update module docstrings

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -12,18 +12,19 @@ def main():
     """Read data and print details of wimbledon champions and what countries have won."""
     data = read_data(FILENAME)
     champion_wins, countries = process_data(data)
-    display_champion_wins(champion_wins)
+    display_results(champion_wins, countries)
+
 
 
 def read_data(filename):
-    """read data from csv."""
-    with open(filename, encoding="utf-8-sig") as infile:
+    """read data from a csv file and return list (excluding header)."""
+    with open(filename,"r", encoding="utf-8-sig") as infile:
         infile.readline()
         lines = [line.strip().split(',') for line in infile]
     return lines
 
 def process_data(data):
-    """process data from csv."""
+    """process data from csv into win counts and country list."""
     champion_wins = {}
     countries = set()
     for row in data:
@@ -33,6 +34,13 @@ def process_data(data):
         champion_wins[champion] = champion_wins.get(champion, 0) + 1
     return champion_wins,countries
 
+def display_results(champion_wins, countries):
+    """display champion win results with countries."""
+    print("Wimbledon Champions:")
+    for name, count in champion_wins.items():
+        print(f"{name}: {count}")
+    print(f"\nThese {len(countries)} countries have won Wimbledon: ")
+    print(", ".join(sorted(countries)))
 
 
 main()
