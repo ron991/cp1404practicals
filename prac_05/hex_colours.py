774558ac8d61852034e3af47d467d835433822d8
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-22 13:00:42 +1000

    Add constant dictionary to program

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -4,6 +4,25 @@ Colour codes in a dictionary:
 """
 
 
+COLOUR_NAME_TO_HEXADECIMAL_CODE = {
+    "AbsoluteZero" : 	"#0048ba",
+    "Acid Green":    "#b0bf1a",
+    "AliceBlue" : "#f0f8ff",
+    "Alizarin crimson": "#e32636",
+    "Amaranth" : "#e52b50",
+    "Amber" : "#ffbf00",
+    "Amethyst" : "#9966cc",
+    "AntiqueWhite" : "#faebd7",
+    "AntiqueWhite1" : "#ffefdb",
+    "Apricot" : "#fbceb1"
+
+
+}
+
+
 def main():
     """Run a hexadecimal colour lookup program"""
-    
+
+
+
+()
