fc01c50a82db45cfed7df648c7b2b8a46aeb4326
rkell <ronald.kelly@my.jcu.edu.au>
2025-06-22 16:48:57 +1000

    Create new function to load data

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,18 @@
+"""
+Wimbledon
+Estimated time to complete: 2 hours
+Actual time to complete:
+"""
+
+FILENAME = "wimbledon.csv"
+
+
+def main():
+    """Read data and print details of wimbledon champions and what countries have won."""
+
+
+main()
+
+
+def load_data(filename):
+    """Load data from csv."""
