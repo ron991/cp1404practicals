32e2793da7ec65ec882bafba4a375af90a36b315
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-05 16:32:34 +1000

    Add code from programming_language.py to main function

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -12,7 +12,14 @@ def main():
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [ruby, visual_basic, python]
+    print(languages)
     print(python)
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
 
 
 main()
