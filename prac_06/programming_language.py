154e6090fe8b3b907bd0ceda632d8aed8cd019ea
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-05 16:19:30 +1000

    Add string constructor to class ProgrammingLanguage

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -14,3 +14,7 @@ class ProgrammingLanguage:
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def __str__(self):
+        """Return a string representation of the object."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
