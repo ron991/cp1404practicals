86e1e184e07bdf93cac45fb08daa2dbbac1397fa
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-05 15:57:04 +1000

    Create class for storing programming language details

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -8,3 +8,9 @@ Actual Time to complete:
 class ProgrammingLanguage:
     """Information about programming language."""
 
+    def __init__(self, name, typing, reflection, year ):
+        """Create a ProgrammingLanguage object."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
