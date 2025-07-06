cd1dc722ea2766fa830584e8315dfbcaa0be4dd4
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-06 14:46:53 +1000

    Add docstring and main function

---
+++ prac_06/guitars.py
@@ -0,0 +1,20 @@
+"""
+Guitars program
+"""
+# My guitars!
+# Name: Fender Stratocaster
+# Year: 2014
+# Cost: $765.4
+# Fender Stratocaster (2014) : $765.40 added.
+# Name:
+# ... snip ...
+# These are my guitars:
+# Guitar 1:  Fender Stratocaster (2014), worth $    765.40
+# Guitar 2:       Gibson L-5 CES (1922), worth $ 16,035.40 (vintage)
+# Guitar 3:        Line 6 JTV-59 (2010), worth $  1,512.90
+
+def main():
+    """Run and list guitars using Guitar class."""
+
+
+main()
