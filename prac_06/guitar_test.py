a8dfcce08a21d60652f885601b76c9e14a5356d4
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-06 14:14:02 +1000

    Removed commented code from prac_06

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -4,11 +4,6 @@ Guitar tests from guitar.py
 
 from prac_06.guitar import Guitar, CURRENT_YEAR
 
-#
-# name = "Gibson L-5 CES"
-# year = 1922
-# cost = 16035.40
-# print(f"My guitar: {name}, first made in {year}")
 
 CURRENT_YEAR = 2025
 
