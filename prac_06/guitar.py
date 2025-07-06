10e4f67e79dec5a4625e34ea06ca0b6331ae13a0
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-06 12:51:08 +1000

    Create string constructor for class Guitar

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -12,3 +12,7 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Represent a string Guitar instance."""
+        return f"{self.name} {self.year} : ${self.cost}"
