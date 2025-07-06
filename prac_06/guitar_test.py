e5ddde6b710e0bfc7ed816ea97c68c1a84f6d69b
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-06 13:56:23 +1000

    Add import statement from guitar.py and constant year as 2025

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -2,3 +2,16 @@
 Guitar tests from guitar.py
 """
 
+from prac_06.guitar import Guitar, CURRENT_YEAR
+#
+# name = "Gibson L-5 CES"
+# year = 1922
+# cost = 16035.40
+# print(f"My guitar: {name}, first made in {year}")
+
+CURRENT_YEAR = 2025
+
+
+def run_tests():
+    """Test for guitar class."""
+
