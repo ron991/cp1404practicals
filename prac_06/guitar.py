a9d026cc7ef5b07f43d1f3f1d61293f48641b908
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-06 12:45:22 +1000

    Create initial constructor for class guitar

---
+++ prac_06/guitar.py
@@ -0,0 +1,14 @@
+"""
+Guitar class for guitar test and guitars program
+"""
+
+
+class Guitar:
+    """Guitar class for guitar information."""
+
+
+    def __init__(self, name ="", year=0, cost=0):
+        """Initialise a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
