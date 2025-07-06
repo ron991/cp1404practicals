86c4d1554d9471527ea1b43c0f9cf2d2b8864a99
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-06 13:45:14 +1000

    Add constants and reformat lines of code

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -2,12 +2,14 @@
 Guitar class for guitar test and guitars program
 """
 
+CURRENT_YEAR = 2025
+VINTAGE_AGE = 50
+
 
 class Guitar:
     """Guitar class for guitar information."""
 
-
-    def __init__(self, name ="", year=0, cost=0):
+    def __init__(self, name="", year=0, cost=0):
         """Initialise a Guitar instance."""
         self.name = name
         self.year = year
@@ -19,6 +21,7 @@ class Guitar:
 
     def get_age(self):
         """Get the age of the Guitar."""
-        return
+        return CURRENT_YEAR - self.year
 
     def is_vintage(self):
+        return self.get_age() >= VINTAGE_AGE
