f75dbdd24c201d34ec8cdab37de8fe2248495d86
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-06 14:59:19 +1000

    Add user input to main function

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -13,8 +13,24 @@ Guitars program
 # Guitar 2:       Gibson L-5 CES (1922), worth $ 16,035.40 (vintage)
 # Guitar 3:        Line 6 JTV-59 (2010), worth $  1,512.90
 
+from prac_06.guitar import Guitar
+
 def main():
     """Run and list guitars using Guitar class."""
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        try:
+            name = input("Name: ")
+        except NameError:
+            print("Name must be a string.")
+
+    year = input("Year: ")
+    while year <= 0:
+        try:
+            year = int(input("Year: "))
+        except ValueError:
+            print("Year must be a number.")
 
 
 main()
