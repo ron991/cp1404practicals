255be5730131fa4c32d1e85c96060c3afc82e929
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-06 14:10:19 +1000

    Add test code to test get_age function from guitar class

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -3,6 +3,7 @@ Guitar tests from guitar.py
 """
 
 from prac_06.guitar import Guitar, CURRENT_YEAR
+
 #
 # name = "Gibson L-5 CES"
 # year = 1922
@@ -14,4 +15,16 @@ CURRENT_YEAR = 2025
 
 def run_tests():
     """Test for guitar class."""
+    name = "Gibson L-5 CES"
+    year = 1922
+    cost = 16035.40
 
+    guitar = Guitar(name,year,cost)
+
+    print(f"{guitar.name} get_age() - Expected {100}. Got {guitar.get_age()}")
+
+
+
+
+if __name__ == "__main__":
+    run_tests()
