facbb8409c6adbfbba273663b53c3db8b6224fbc
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-19 22:41:14 +1000

    Edit code to allow for point_arithmetic detail

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -26,9 +26,9 @@ class ProgrammingLanguage:
 
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True,True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False,False, 1991)
 
     languages = [ruby, python, visual_basic]
     print(python)
