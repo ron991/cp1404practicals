799686a301775e128264613a575d69f4b0dc930a
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-20 22:49:35 +1000

    Add import statements and create main function

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -4,5 +4,12 @@ Estimated time : 3 hours
 Actual time :   hours
 """
 
+import csv
+
+from guitar import Guitar
+
+def main():
+    """Read file of guitars and store them in a list of guitar objects"""
 
 
+main()
