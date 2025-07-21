ad1a8fc1c45a74590c3aef4ea0bf9fb0734001ea
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-21 21:47:26 +1000

    Move guitar sort to outside the for loop

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -8,10 +8,11 @@ import csv
 
 from guitar import Guitar
 
+
 def main():
     """Read file of guitars and store them in a list of guitar objects"""
     guitars = []
-    with open('guitars.csv', 'r', newline= '') as in_file:
+    with open('guitars.csv', 'r', newline='') as in_file:
         reader = csv.reader(in_file)
         for row in reader:
             name = row[0]
@@ -19,10 +20,10 @@ def main():
             cost = float(row[2])
 
             guitars.append(Guitar(name, year, cost))
+    guitars.sort()
 
-        for guitar in guitars:
-            #guitars.sort()
-            print(guitar)
+    for guitar in guitars:
+        print(guitar)
 
 
 main()
