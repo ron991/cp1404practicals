6444ab6d0e57d3061182286d07ed1467dfe42170
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-21 21:33:36 +1000

    Add with open file to main function

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -10,6 +10,16 @@ from guitar import Guitar
 
 def main():
     """Read file of guitars and store them in a list of guitar objects"""
+    guitars = []
+    with open('guitars.csv', 'r', newline= '') as in_file:
+        reader = csv.reader(in_file)
+        for row in reader:
+            name, year, cost = row
+            guitar = Guitar(name, year, cost)
+            guitars.append(guitar)
+
+        for guitar in guitars:
+            print(guitar)
 
 
 main()
