1bb07ad750972bedfe44022ecc509d1757b84d94
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-28 21:23:53 +1000

    Add new text output to box_layout.kv

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -10,6 +10,8 @@ class BoxLayoutDemo(App):
 
     def handle_greet(self):
         print("Greet")
+        print("test")
+        self.root.ids.output_label.text = "Hello "
 
 
 BoxLayoutDemo().run()
