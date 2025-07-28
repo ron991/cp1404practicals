a5b37af296a9acc77221dc6506bc85ee228ed374
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-28 22:51:19 +1000

    Create class ConvertMiles and attributes

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -6,3 +6,10 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 
+class ConvertMiles(App):
+    output_km = StringProperty()
+
+    def build(self):
+        """Build Kivy app"""
+        self.title = "Convert Miles to Kilometers"
+        self.root = Builder.load_file('convert_miles_km.kv')
