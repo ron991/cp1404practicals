a883a7192b185529e50883c5d47b195f8ee1ab58
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-28 22:04:50 +1000

    Add docstrings to functions

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -4,16 +4,19 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """Build box layout demo"""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Handle greet button"""
         print("Greet")
         print("test")
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Handle clear button"""
         self.root.ids.output_label.text = ""
         self.root.ids.output_label.text = ""
 
