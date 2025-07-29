c0a216634485b68ca49cbd17a9dda53fd0bc8a8d
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-29 22:08:41 +1000

    Add Docstring and import statements

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,11 @@
+"""
+Dynamic labels Week 8 practical
+"""
+
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+from kivy.properties import StringProperty
+
+
