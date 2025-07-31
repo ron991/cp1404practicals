dae1b193cea03c1b1bed54baba01b123bac902cf
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-31 14:10:02 +1000

    Add attributes to taxi object

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,15 @@
+"""
+Taxi test code
+"""
+
+
+from prac_09.taxi import Taxi
+
+def main():
+    """Test taxi class."""
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi.drive(40)
+    print(my_taxi)
+
+
+main()
