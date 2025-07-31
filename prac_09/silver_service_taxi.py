db513fbae9becb445d14791a1e5c6858e6f66ccb
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-31 16:47:15 +1000

    Create constructor for silver_service_taxi.py

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,16 @@
+"""
+Silver Service Taxi class
+"""
+
+from prac_09.taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    """A silver service taxi class from taxi"""
+
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialize the Silver Service taxi class"""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
