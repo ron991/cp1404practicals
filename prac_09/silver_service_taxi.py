266220569582d0202ad78d1d072cebb5a84cbe9f
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-31 17:13:34 +1000

    Create function to get fare

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -6,6 +6,7 @@ from prac_09.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """A silver service taxi class from taxi"""
+    flagfall = 4.50
 
 
     def __init__(self, name, fuel, fanciness):
@@ -13,4 +14,8 @@ class SilverServiceTaxi(Taxi):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km *= fanciness
+
+    def get_fare(self):
+        """Calculate the fare"""
+        return self.flagfall + super().get_fare()
 
