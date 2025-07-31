d489d1872029b35205c19fa421c9bb136f2a70a3
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-31 16:36:00 +1000

    Fix random number for self reliability

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -9,13 +9,14 @@ def main():
     """Test the unreliable car."""
 
     bad_car = UnreliableCar("Bomb", 100, 10)
-    average_car = UnreliableCar("toyota", 100, 80)
+    average_car = UnreliableCar("toyota", 100, 90)
 
 
-    for i in range(1,12):
+    for i in range(1,11):
         print(f"Test drive {i}km: ")
-        print(f"{bad_car.name:12} drove {bad_car.drive(i):2}km")
         print(f"{average_car.name:12} drove {average_car.drive(i):2}km")
+        print(f"{bad_car.name:12} drove {bad_car.drive(i):2}km")
+
 
     print(bad_car)
     print(average_car)
