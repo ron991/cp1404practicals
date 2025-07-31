b55af1f527675639e1219d616053925663d318a7
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-31 14:14:40 +1000

    Add class variable and changed client code

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -7,7 +7,7 @@ from prac_09.taxi import Taxi
 
 def main():
     """Test taxi class."""
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(my_taxi)
     my_taxi.start_fare()
