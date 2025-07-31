2500e8463440b2e070ea1652ddbbc2c90331f65c
rkell <ronald.kelly@my.jcu.edu.au>
2025-07-31 16:17:23 +1000

    Add Test code for unreliable_car_test.py

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -1,1 +1,20 @@
+"""
+Test the unreliable car.
+"""
 
+
+from prac_09.unreliable_car import UnreliableCar
+
+def main():
+    """Test the unreliable car."""
+
+    bad_car = UnreliableCar("Needs work", 100, 30)
+
+    for i in range(1,10):
+        print(f"Test drive {i}km: ")
+        print(f"{bad_car:12} drove {bad_car.drive(i):2}km")
+
+    print(bad_car)
+
+
+main()
