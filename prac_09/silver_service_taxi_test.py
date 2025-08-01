5952955eb0ebfd571afe93e0f2a0bd92253ebca4
rkell <ronald.kelly@my.jcu.edu.au>
2025-08-01 17:49:54 +1000

    Complete main function

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,10 +1,14 @@
 """
-Silver service taxi tests
+Silver service taxi tests.
 """
 
 from  prac_09.silver_service_taxi import SilverServiceTaxi
 
 def main():
-    """Test drive the silver service taxi"""
+    """Test drive the silver service taxi."""
     taxi = SilverServiceTaxi('Hummer', 200, 4)
     taxi.drive(100)
+    print(taxi)
+
+
+main()
