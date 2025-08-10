888e201ae3c7743caa5b3f6c360d8bce0279e67e
rkell <ronald.kelly@my.jcu.edu.au>
2025-08-10 15:27:57 +1000

    Complete wiki.py program

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -8,12 +8,29 @@ import wikipedia
 def main():
     """Search for a wikipedia page"""
 
-    while True:
-        title = wikipedia.search("Wikipedia")
-        if title:
-            page = wikipedia.page(title)
-            content = page.content
-            print(content)
+    print("Wikipedia page search tool")
+    title = input("Enter a page title or search phrase: ")
+    while title != "":
+        try:
+            page = wikipedia.page(title, auto_suggest=False)
+            print("Page title:", page.title)
+            print("Page content:", page.content)
+            print("Page url:", page.url)
+            print("Page summary:", page.summary)
+
+        except wikipedia.exceptions.PageError:
+            print("Page not found")
+        except wikipedia.exceptions.DisambiguationError:
+            print("Page not found")
+        except wikipedia.exceptions.RedirectError:
+            print("Page not found")
+
+        title = input("Enter a page title or search phrase: ")
+
+    print("Goodbye")
+
+if __name__ == "__main__":
+    main()
 
 
 main()
