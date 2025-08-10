9e60ff10a6a5ac74dcbc48db44ec33f42f19f665
rkell <ronald.kelly@my.jcu.edu.au>
2025-08-10 15:21:25 +1000

    Add test code for import statement

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -4,3 +4,16 @@ CP1404 Week 10 wikipedia page program
 
 
 import wikipedia
+
+def main():
+    """Search for a wikipedia page"""
+
+    while True:
+        title = wikipedia.search("Wikipedia")
+        if title:
+            page = wikipedia.page(title)
+            content = page.content
+            print(content)
+
+
+main()
